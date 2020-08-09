69c596e71f8372e01ad896db933f57f573bdd504
sweetpants <artm-pakentreyger@hotmail.com>
2020-08-09 20:48:32 +0300

    case2

--- lesson01/lesson01_cases.py
+++ lesson01/lesson01_cases.py
@@ -2,10 +2,21 @@
 # Поработайте с переменными, создайте несколько, выведите на экран,
 # запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
 
-var1 = 7
-var2 = 42
-var3 = "Artem"
-print(var1, var2, var3)
+var1_int = int(7)
+var2_float = float(42.88)
+var3_str = str("Artem 1991")
+print(var1_int, var2_float, var3_str)
 
 var4 = input("Text here something - ")
 print("You text", var4)
+
+# case2:
+# Пользователь вводит время в секундах.
+# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
+time = int(input("Enter your time in seconds here: "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = (time - (hours * 3600 + minutes * 60))
+print(f"Time format hh:mm:ss {hours} : {minutes} : {seconds}")
